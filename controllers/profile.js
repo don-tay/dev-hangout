39c697600a7bbf6e7dec22a0e19be751d06b1cdc
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-06-20 20:23:07 +0800

    Add create profile method

--- controllers/profile.js
+++ controllers/profile.js
@@ -23,3 +23,30 @@ exports.getLoggedInProfile = async (req, res) => {
     res.status(500).send('Server Error');
   }
 };
+
+// @route   POST api/profile
+// @desc    Create user profile
+// @access  Private
+exports.createProfile = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+
+    // If user profile exist, cannot create another profile
+    if (profile) {
+      return res.status(400).json('User profile already exists.');
+    }
+
+    // Create
+    req.body.user = req.user.id;
+    profile = await Profile.create(req.body);
+    res.status(200).json(profile);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+};
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 
 const { auth } = require('../../middleware/auth');
-const { getLoggedInProfile } = require('../../controllers/profile');
+const {
+  getLoggedInProfile,
+  createProfile
+} = require('../../controllers/profile');
 
 router.get('/me', auth, getLoggedInProfile);
+
+router.post(
+  '/',
+  auth,
+  [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+  ],
+  createProfile
+);
 
 module.exports = router;
